839a4df5dacefd6a5ed6588f6351bc62f628ce35
scolphew <scolphew@pku.edu.cn>
2016-08-24 11:27:26 +0800

    一次提交

--- src/lsf/_058_lengthofLastWord/Solution.java
+++ src/lsf/_058_lengthofLastWord/Solution.java
@@ -6,7 +6,6 @@ public class Solution {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		Solution s=new Solution();
 		System.out.println( s.lengthOfLastWord("hhh sssa ")  );
 	}
