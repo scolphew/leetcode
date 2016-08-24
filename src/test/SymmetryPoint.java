d1086907b2006db86e101691b5f24f227a8c40ed
scolphew <scolphew@pku.edu.cn>
2016-08-24 14:15:11 +0800

    ok

--- src/test/SymmetryPoint.java
+++ src/test/SymmetryPoint.java
@@ -9,13 +9,14 @@ public class SymmetryPoint {
     /**
      * 给定平面上的n个点，问是否存在一条平行于y轴的直线使得这些点关于直线对称
      *
-     * @param points
+     * @param points 输入点
      */
     public void solution(int[][] points) {
-
+        // TODO: 2016/8/24
     }
 
     public static void main(String[] args) {
+        // TODO: 2016/8/24
     }
 
 }
