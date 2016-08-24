069749796816a3978ae7737718344e37f68b0fdf
scolphew <scolphew@pku.edu.cn>
2016-08-24 14:28:35 +0800

    强迫症

--- src/test/SymmetryPoint.java
+++ src/test/SymmetryPoint.java
@@ -2,21 +2,24 @@ package test;
 
 /**
  * @author scolphew
- *         骞抽潰涓婄偣鍏充簬鐩寸嚎瀵圭О
+ *         平面上点关于直线对称
  */
 public class SymmetryPoint {
 
     /**
-     * 缁欏畾骞抽潰涓婄殑n涓偣锛岄棶鏄惁瀛樺湪涓?鏉″钩琛屼簬y杞寸殑鐩寸嚎浣垮緱杩欎簺鐐瑰叧浜庣洿绾垮绉?
+     * 给定平面上的n个点，问是否存在一条平行于y轴的直线使得这些点关于直线对称
      *
-     * @param points 杈撳叆鐐?
+     * @param points 输入点
      */
     public void solution(int[][] points) {
-        // TODO: 2016/8/24 鍒氭墠
+        // TODO: 2016/8/24
+        System.out.println(points.length);
     }
 
     public static void main(String[] args) {
         // TODO: 2016/8/24
+        SymmetryPoint symmetryPoint = new SymmetryPoint();
+        symmetryPoint.solution(new int[5][2]);
     }
 
 }
