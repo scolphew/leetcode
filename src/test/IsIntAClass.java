0e55b5e8517deb2bdceb4af13714af258849a76d
scolphew <scolphew@pku.edu.cn>
2016-09-04 11:58:00 +0800

    ...................................

---
+++ src/test/IsIntAClass.java
@@ -0,0 +1,13 @@
+package test;
+
+/**
+ * @author scolphew
+ */
+public class IsIntAClass {
+    public static void main(String[] args) {
+        int a = 0;
+        Integer b = new Integer(5);
+
+        System.out.println(b.getClass());
+    }
+}
