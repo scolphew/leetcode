2e189e453f702cb6754cf2e3fa9bdebb62621f90
scolphew <scolphew@pku.edu.cn>
2016-09-08 10:45:12 +0800

    14题 1

--- src/lsf/_014_LongestCommonPrefix/Solution.java
+++ src/lsf/_014_LongestCommonPrefix/Solution.java
@@ -12,7 +12,28 @@ public class Solution {
      * @return
      */
     public String longestCommonPrefix(String[] strs) {
+        if(strs==null || strs.length==0)
+            return "";
+        for (int i = 1; i < strs.length; i++) {
+            strs[0] = commonPrefix(strs[0],strs[i]);
+        }
+        return strs[0];
+    }
 
-        return null;
+    public String commonPrefix(String s1, String s2){
+        StringBuilder sb = new StringBuilder();
+        for(int i=0,j=0;i<s1.length() && j<s2.length();++i,++j){
+            if(s1.charAt(i) == s2.charAt(j))
+                sb.append(s1.charAt(i));
+            else
+                break;
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.longestCommonPrefix(new String[]{"abcasasa","abd","abc"}));
+        System.out.println(s.longestCommonPrefix(new String[]{}));
     }
 }
