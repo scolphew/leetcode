6f2e124ab95b00c874635455b869692e2aeebe03
scolphew <scolphew@pku.edu.cn>
2016-09-11 10:31:21 +0800

    add
    add

--- src/test/SymmetryPoint.java
+++ src/test/SymmetryPoint.java
@@ -16,6 +16,10 @@ public class SymmetryPoint {
         System.out.println(123);
     }
 
+/**
+     * allalaalalalla
+     * @param args
+     */
     public static void main(String[] args) {
         // TODO: 2016/8/24
 
