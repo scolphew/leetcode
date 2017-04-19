ef0a3b0f3f28c8ae3e51dfa36bd6e985cee9a2f3
scolphew <scolphew@pku.edu.cn>
2017-04-19 10:39:46 +0800

    142寻找链表的环，找到初始点或null

--- src/base/ListNode.java
+++ src/base/ListNode.java
@@ -1,46 +1,51 @@
 package base;
 
 public class ListNode {
-	
-	public int val;
-	
-	public ListNode next;
-	private ListNode(){}
-	public ListNode(int x) {
-		val = x;
-	}
-	
 
-	public static ListNode scanf(int[] i) {
-		if(i.length==0)
-			return null;
-		ListNode head = new ListNode(i[0]);
-		ListNode per = head;
-		for (int j = 1; j < i.length; j++) {
-			ListNode x = new ListNode(i[j]);
-			per.next = x;
-			per = x;
-		}
-		return head;
-	}
+    public int val;
 
-	public static void print(ListNode head) {
-		ListNode per = head;
-		while (per != null) {
-			System.out.print(per.val + " ");
-			per = per.next;
-		}
-		System.out.println();
-	}
+    public ListNode next;
 
-	@Override
-	public String toString() {
-		StringBuilder sb =new StringBuilder();
-		ListNode per = this;
-		while (per != null) {
-            sb.append(per.val+"   ");
-			per = per.next;
-		}
-		return sb.toString();
-	}
+    private ListNode() {
+    }
+
+    public ListNode(int x) {
+        val = x;
+    }
+
+
+    public static ListNode scanf(int[] i) {
+        if (i.length == 0)
+            return null;
+        ListNode head = new ListNode(i[0]);
+        ListNode per = head;
+        for (int j = 1; j < i.length; j++) {
+            ListNode x = new ListNode(i[j]);
+            per.next = x;
+            per = x;
+        }
+        return head;
+    }
+
+    public static void print(ListNode head, int i) {
+        ListNode per = head;
+        int index = 0;
+        while (per != null && index++ < i) {
+            System.out.print(per.val + " ");
+            per = per.next;
+        }
+        System.out.println();
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        ListNode per = this;
+        int index = 1;
+        while (per != null && index++ < 10) {
+            sb.append(per.val + "   ");
+            per = per.next;
+        }
+        return sb.toString();
+    }
 }
