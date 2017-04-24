709c655ab8aac0d99e6c2aff3c427bfe77047590
scolphew <scolphew@pku.edu.cn>
2017-04-24 15:27:00 +0800

    168 excel 列号 如1 -> A，28 -> AB

---
+++ src/lsf/_168_ExcelSheetColumnTitle/Solution.java
@@ -0,0 +1,22 @@
+package lsf._168_ExcelSheetColumnTitle;
+
+public class Solution {
+    public String convertToTitle(int n) {
+        return n == 0 ? "" : convertToTitle(--n / 26) + (char) ('A' + (n % 26));
+    }
+
+    public String convertToTitle2(int n) {
+        StringBuilder sb = new StringBuilder();
+        char c;
+        while (n > 0) {
+            c = (char) (((--n) % 26) + 'A');
+            sb.insert(0, c);
+            n /= 26;
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Solution().convertToTitle2(26));
+    }
+}
