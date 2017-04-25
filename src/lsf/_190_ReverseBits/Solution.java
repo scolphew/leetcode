c00a5050423be0d481d4f05ea7463bb110c845f4
scolphew <scolphew@pku.edu.cn>
2017-04-25 16:07:02 +0800

    190 反转二进制

---
+++ src/lsf/_190_ReverseBits/Solution.java
@@ -0,0 +1,20 @@
+package lsf._190_ReverseBits;
+
+public class Solution {
+    public int reverseBits(int n) {
+        int res = 0;
+        int i = 32;
+        while (i-- > 0) {
+            res = (res << 1) | (n & 1);
+            n >>= 1;
+        }
+        return res;
+    }
+
+
+    public static void main(String[] args) {
+        int b = 43261596;
+        b = new Solution().reverseBits(b);
+        System.out.println(b);
+    }
+}
