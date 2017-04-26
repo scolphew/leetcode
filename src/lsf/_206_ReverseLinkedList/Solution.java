33f991136f0b053fc90f7f9a5b375a15067c6cbd
scolphew <scolphew@pku.edu.cn>
2017-04-26 10:26:17 +0800

    206 ...

---
+++ src/lsf/_206_ReverseLinkedList/Solution.java
@@ -0,0 +1,16 @@
+package lsf._206_ReverseLinkedList;
+
+import base.ListNode;
+
+public class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode pre = null, tmp;
+        while (head != null) {
+            tmp = head.next;
+            head.next = pre;
+            pre = head;
+            head = tmp;
+        }
+        return pre;
+    }
+}
