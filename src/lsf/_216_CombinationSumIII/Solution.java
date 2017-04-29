3b1a368e53b5946c6c5d07b8d7ade635602efd58
scolphew <scolphew@pku.edu.cn>
2017-04-29 16:18:17 +0800

    216  k个树，之和为n，求组合 其中k个数字不重复，1-9之间

---
+++ src/lsf/_216_CombinationSumIII/Solution.java
@@ -0,0 +1,34 @@
+package lsf._216_CombinationSumIII;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+
+    public List<List<Integer>> combinationSum3(int k, int n) {
+        List<List<Integer>> res = new ArrayList<>();
+        foo(res, new ArrayList<>(), 1, k, n);
+        return res;
+    }
+
+    public void foo(List<List<Integer>> ans, List<Integer> pre, int start, int k, int n) {
+        if (k == 1) {
+            if (start <= n && n <= 9) {
+                List<Integer> tmp = new ArrayList<>(pre);
+                tmp.add(n);
+                ans.add(tmp);
+            }
+            return;
+        }
+        for (int i = start; i <= 9; i++) {
+            List<Integer> tmp = new ArrayList<>(pre);
+            tmp.add(i);
+            foo(ans, tmp, i + 1, k - 1, n - i);
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Solution().combinationSum3(3, 7));
+    }
+
+}
