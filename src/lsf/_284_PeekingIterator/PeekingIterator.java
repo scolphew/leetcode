71024ca993490f11534c859f9ea6b4d9b837e9ea
scolphew <scolphew@pku.edu.cn>
2017-05-09 14:35:22 +0800

    284 可以peek的迭代器Iterator

---
+++ src/lsf/_284_PeekingIterator/PeekingIterator.java
@@ -0,0 +1,32 @@
+package lsf._284_PeekingIterator;
+
+import java.util.Iterator;
+
+public class PeekingIterator implements Iterator<Integer> {
+
+    private Integer next = null;
+    private Iterator<Integer> iter;
+
+    public PeekingIterator(Iterator<Integer> iterator) {
+        iter = iterator;
+        if (iter.hasNext())
+            next = iter.next();
+    }
+
+    public Integer peek() {
+        return next;
+    }
+
+    @Override
+    public Integer next() {
+        Integer tmp = next;
+        next = iter.hasNext() ? iter.next() : null;
+        return tmp;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return next != null;
+    }
+
+}
