d7ef24ce59b7c176123fe9605af2311403b46d91
scolphew <scolphew@pku.edu.cn>
2017-05-09 23:01:19 +0800

    298 求中位数的在线算法，（插入和获取交叉调用） 优先队列

---
+++ src/lsf/_295_FindMedianfromDataStream/MedianFinder.java
@@ -0,0 +1,24 @@
+package lsf._295_FindMedianfromDataStream;
+
+import java.util.PriorityQueue;
+
+public class MedianFinder {
+
+    private PriorityQueue<Long> small = new PriorityQueue(),
+            large = new PriorityQueue();
+
+    public void addNum(int num) {
+        large.offer((long) num);
+        small.offer(-large.poll());
+        if (large.size() < small.size())
+            large.add(-small.poll());
+    }
+
+    public double findMedian() {
+        return large.size() > small.size()
+                ? large.peek()
+                : (large.peek() - small.peek()) / 2.0;
+    }
+
+
+}
