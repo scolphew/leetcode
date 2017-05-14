7ee95467a5306c4232f7b56d5edef20ef3850f16
scolphew <scolphew@pku.edu.cn>
2017-05-14 16:04:10 +0800

    587  求所有点的最外围的点，（用最小的长度的线把所有的点围起来，返回所有外围的点）

---
+++ src/base/Point.java
@@ -0,0 +1,16 @@
+package base;
+
+public class Point {
+    public int x;
+    public int y;
+
+    public Point() {
+
+    }
+
+    public Point(int a, int b) {
+        x = a;
+        y = b;
+    }
+
+}
