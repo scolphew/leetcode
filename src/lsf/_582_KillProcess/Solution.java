064e074616824d84444adaaa972ec0f39406eb5b
scolphew <scolphew@pku.edu.cn>
2017-05-16 00:21:26 +0800

    528 给出pid和ppid，各一个需要杀死的进程id，求最终要杀死的所有进程id（一个县城被杀死，则他的子线程同样被杀死）

---
+++ src/lsf/_582_KillProcess/Solution.java
@@ -0,0 +1,26 @@
+package lsf._582_KillProcess;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class Solution {
+    public List<Integer> killProcess(List<Integer> pid, List<Integer> ppid, int kill) {
+        List<Integer> ans = new ArrayList<>();
+
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(kill);
+        while (!queue.isEmpty()) {
+            int tmp = queue.poll();
+            ans.add(tmp);
+            for (int i = 0; i < pid.size(); i++) {
+                if (ppid.get(i) == tmp) {
+                    queue.add(pid.get(i));
+                }
+            }
+        }
+        return ans;
+    }
+
+}
