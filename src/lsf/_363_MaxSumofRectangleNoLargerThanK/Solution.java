d1abb48df0254b244bd2aa9312aaf178c87a9134
scolphew <scolphew@pku.edu.cn>
2017-05-16 10:50:02 +0800

    363 求二维矩阵中矩形区域的和<=targetd的最大值 TreeSet

---
+++ src/lsf/_363_MaxSumofRectangleNoLargerThanK/Solution.java
@@ -0,0 +1,38 @@
+package lsf._363_MaxSumofRectangleNoLargerThanK;
+
+import java.util.TreeSet;
+
+public class Solution {
+    public int maxSumSubmatrix(int[][] matrix, int target) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int res = Integer.MIN_VALUE;
+        for (int i = 0; i < m; i++) {
+            int[] array = new int[n];
+            for (int j = i; j >= 0; j--) {
+                int val = 0;
+                TreeSet<Integer> set = new TreeSet<Integer>();
+                set.add(0);
+                for (int k = 0; k < n; k++) {
+                    array[k] = array[k] + matrix[j][k];
+                    val = val + array[k];
+                    Integer subres = set.ceiling(val - target);
+                    if (null != subres) {
+                        res = Math.max(res, val - subres);
+                    }
+                    set.add(val);
+                }
+            }
+        }
+        return res;
+
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(new Solution().maxSumSubmatrix(new int[][]{
+                {1, 0, 1},
+                {0, -2, 3}
+        }, 2));
+    }
+}
