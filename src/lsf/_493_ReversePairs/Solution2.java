4be084bfcbe850bdcff3742193208abdec814838
scolphew <scolphew@pku.edu.cn>
2017-05-17 20:53:13 +0800

    493 求有几对ij满足i<j && A[i]>2A[j] 归并 修改归并的数组，使用全局的tmp数组，减少归并的开销

--- src/lsf/_493_ReversePairs/Solution2.java
+++ src/lsf/_493_ReversePairs/Solution2.java
@@ -2,20 +2,20 @@ package lsf._493_ReversePairs;
 
 public class Solution2 {
     public int reversePairs(int[] nums) {
-        return merge(nums, 0, nums.length - 1);
+        int[] tmp = new int[nums.length];
+        return merge(nums, tmp, 0, nums.length - 1);
     }
 
-    private int merge(int[] nums, int s, int e) {
+    private int merge(int[] nums, int[] tmp, int s, int e) {
         if (s >= e) return 0;
         int mid = s + (e - s) / 2;
-        int cnt = merge(nums, s, mid) + merge(nums, mid + 1, e);
-        int tmp[] = new int[e - s + 1];
+        int cnt = merge(nums, tmp, s, mid) + merge(nums, tmp, mid + 1, e);
         for (int i = s, j = mid + 1; i <= mid; i++) {
             while (j <= e && nums[i] / 2.0 > nums[j])
                 j++;
             cnt += j - mid - 1;
         }
-        int i = s, j = mid + 1, index = 0;
+        int i = s, j = mid + 1, index = s;
         while (i <= mid && j <= e) {
             if (nums[i] < nums[j])
                 tmp[index++] = nums[i++];
@@ -26,7 +26,7 @@ public class Solution2 {
             tmp[index++] = nums[i++];
         while (j <= e)
             tmp[index++] = nums[j++];
-        System.arraycopy(tmp, 0, nums, s, tmp.length);
+        System.arraycopy(tmp, s, nums, s, e - s + 1);
         return cnt;
     }
 
