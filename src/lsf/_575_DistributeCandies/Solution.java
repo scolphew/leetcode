594088e54e4c79d2374a949e1658aae94ea89877
scolphew <scolphew@pku.edu.cn>
2017-05-18 22:23:03 +0800

    575 分糖果  没意思

--- src/lsf/_575_DistributeCandies/Solution.java
+++ src/lsf/_575_DistributeCandies/Solution.java
@@ -10,4 +10,13 @@ public class Solution {
             set.add(candy);
         return set.size() >= candies.length / 2 ? candies.length / 2 : set.size();
     }
+
+    public int function2(int[] candies) {
+        int n = candies.length;
+        Set<Integer> set = new HashSet<Integer>(n);
+        for (int i = 0; i < n && set.size() < n / 2; i++) {
+            set.add(candies[i]);
+        }
+        return set.size();
+    }
 }
