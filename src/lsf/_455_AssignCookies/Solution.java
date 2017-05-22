cb1b434694d6ebd31ee8f549e45c2e7077ccdece
scolphew <scolphew@pku.edu.cn>
2017-05-22 22:50:19 +0800

    455 分饼干，gi代表小朋友的最小需求，sj代表饼干的大小，求最多能满足人数 每个人最多分一块饼，每个饼最多一个人

---
+++ src/lsf/_455_AssignCookies/Solution.java
@@ -0,0 +1,24 @@
+package lsf._455_AssignCookies;
+
+import java.util.Arrays;
+
+public class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int i = 0;
+        for (int j = 0; i < g.length && j < s.length; j++) {
+            if (g[i] <= s[j])
+                i++;
+        }
+        return i;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Solution().findContentChildren(
+                new int[]{10, 9, 8, 7},
+                new int[]{6, 5, 7, 8}
+        ));
+    }
+
+}
