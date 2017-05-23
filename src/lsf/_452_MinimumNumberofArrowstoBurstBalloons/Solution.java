ecc23655899a87fb9c54a3c232b2856360005510
scolphew <scolphew@pku.edu.cn>
2017-05-23 11:51:36 +0800

    452 打气球，给出气球的x坐标范围（a，b），在x轴坐标c向上发射火箭，若a<=c<=b，则打中气球，求最少需要多少火箭才能打中所有气球

--- src/lsf/_452_MinimumNumberofArrowstoBurstBalloons/Solution.java
+++ src/lsf/_452_MinimumNumberofArrowstoBurstBalloons/Solution.java
@@ -1,6 +1,7 @@
 package lsf._452_MinimumNumberofArrowstoBurstBalloons;
 
 import java.util.Arrays;
+import java.util.Comparator;
 
 public class Solution {
     public int findMinArrowShots(int[][] points) {
@@ -20,9 +21,24 @@ public class Solution {
         return ans;
     }
 
+    public int foo(int[][] points) {
+        if (points == null || points.length == 0) return 0;
+        Arrays.sort(points, Comparator.comparingInt(a -> a[1]));
+        int ans = 1;
+        int lastEnd = points[0][1];
+        for (int i = 1; i < points.length; i++) {
+            if (points[i][0] > lastEnd) {
+                ans++;
+                lastEnd = points[i][1];
+            }
+        }
+        return ans;
+    }
+
     public static void main(String[] args) {
         System.out.println(new Solution().findMinArrowShots(new int[][]{{10, 16}, {2, 8}, {1, 6}, {7, 12}}));
         System.out.println(new Solution().findMinArrowShots(new int[][]{{-1, 1}}));
         System.out.println(new Solution().findMinArrowShots(new int[][]{{3, 9}, {7, 12}, {3, 8}, {6, 8}, {9, 10}, {2, 9}, {0, 9}, {3, 9}, {0, 6}, {2, 8}}));
+        System.out.println(new Solution().foo(new int[][]{{8, 12}, {1, 10}, {4, 11}, {8, 12}, {3, 9}, {6, 9}, {6, 97}}));
     }
 }
