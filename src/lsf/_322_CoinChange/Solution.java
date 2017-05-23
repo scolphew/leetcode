055d9d67ddd49e102449497435baf06f099cb788
scolphew <scolphew@pku.edu.cn>
2017-05-23 15:29:53 +0800

    322 硬币(邮票)问题 不可解返回-1

--- src/lsf/_322_CoinChange/Solution.java
+++ src/lsf/_322_CoinChange/Solution.java
@@ -4,6 +4,10 @@ public class Solution {
     public int coinChange(int[] coins, int amount) {
         if (amount == 0)
             return 0;
+        if (coins.length == 1) {
+            return amount % coins[0] == 0 ? amount / coins[0] : -1;
+        }
+
         int[] dp = new int[amount + 1];
         for (int i = 1; i <= amount; i++) {
             dp[i] = Integer.MAX_VALUE;
