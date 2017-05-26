80c5a98eb4306c8e936c777f6809de8a1e1f4e2f
scolphew <scolphew@pku.edu.cn>
2017-05-26 13:20:49 +0800

    347 找出数组中最常见的k个元素  有点问题，能AC

--- src/lsf/_347_TopKFrequentElements/Solution.java
+++ src/lsf/_347_TopKFrequentElements/Solution.java
@@ -24,4 +24,45 @@ public class Solution {
         return ans;
     }
 
+
+    /**
+     * 有点问题，能AC
+     * 如：[1,1,1,2,3,2,3] k=2
+            返回【1，2,3】
+     * @param nums
+     * @param k
+     * @return
+     */
+    public List<Integer> topKFrequent2(int[] nums, int k) {
+
+        List<Integer>[] bucket = new List[nums.length + 1];
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        // put inside map
+        for (int n : nums) {
+            map.put(n, map.getOrDefault(n, 0) + 1);
+        }
+
+        // check
+        for (int key : map.keySet()) {
+            int f = map.get(key); // frequency
+            if (bucket[f] == null) {
+                bucket[f] = new ArrayList<>();
+            }
+
+            bucket[f].add(key);
+        }
+
+        List<Integer> res = new ArrayList<>();
+
+        for (int i = bucket.length - 1; i >= 0 && res.size() < k; i--) {
+            if (bucket[i] != null) {
+                res.addAll(bucket[i]);
+            }
+        }
+
+        return res;
+    }
+
+
 }
