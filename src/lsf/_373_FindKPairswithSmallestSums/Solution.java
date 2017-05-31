d08763a003a28acc173be0347630f28d7aa6c623
scolphew <scolphew@pku.edu.cn>
2017-05-31 13:03:50 +0800

    373 两个有序数组，从每个数组选一个数作为一对数字，求最小的k个

--- src/lsf/_373_FindKPairswithSmallestSums/Solution.java
+++ src/lsf/_373_FindKPairswithSmallestSums/Solution.java
@@ -6,25 +6,34 @@ import java.util.List;
 import java.util.PriorityQueue;
 
 public class Solution {
+    class Pair {
+        int[] nums;
+        int index;
+
+        Pair(int a, int b, int index) {
+            nums = new int[]{a, b};
+            this.index = index;
+        }
+    }
 
 
     public List<int[]> kSmallestPairs(int[] nums1, int[] nums2, int k) {
-        PriorityQueue<int[]> que = new PriorityQueue<>(new Comparator<int[]>() {
+        PriorityQueue<Pair> que = new PriorityQueue<>(new Comparator<Pair>() {
             @Override
-            public int compare(int[] o1, int[] o2) {
-                return o1[0] + o1[1] - o2[0] - o2[1];
+            public int compare(Pair o1, Pair o2) {
+                return o1.nums[0] + o1.nums[1] - o2.nums[0] - o2.nums[1];
             }
         });
         List<int[]> res = new ArrayList<>();
         if (nums1.length == 0 || nums2.length == 0 || k == 0)
             return res;
         for (int i = 0; i < nums1.length && i < k; i++)
-            que.offer(new int[]{nums1[i], nums2[0], 0});
+            que.offer(new Pair(nums1[i], nums2[0], 0));
         while (k-- > 0 && !que.isEmpty()) {
-            int[] cur = que.poll();
-            res.add(new int[]{cur[0], cur[1]});
-            if (cur[2] == nums2.length - 1) continue;
-            que.offer(new int[]{cur[0], nums2[cur[2] + 1], cur[2] + 1});
+            Pair cur = que.poll();
+            res.add(cur.nums);
+            if (cur.index == nums2.length - 1) continue;
+            que.offer(new Pair(cur.nums[0], nums2[cur.index + 1], cur.index + 1));
         }
         return res;
 
