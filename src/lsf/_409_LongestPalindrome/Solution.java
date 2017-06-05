576c87b56bca2adfb1a769add251c734e94feef3
scolphew <scolphew@pku.edu.cn>
2017-06-05 15:16:14 +0800

    409 string中选择字符，组成最长回文长度

--- src/lsf/_409_LongestPalindrome/Solution.java
+++ src/lsf/_409_LongestPalindrome/Solution.java
@@ -6,15 +6,13 @@ public class Solution {
         for (char c : s.toCharArray())
             map[c]++;
         int ans = 0;
-        boolean flag = false;
         for (int i : map) {
             if (i % 2 == 0)
                 ans += i;
             else {
                 ans += i - 1;
-                flag = true;
             }
         }
-        return ans + (flag ? 1 : 0);
+        return ans + (s.length()>ans ? 1 : 0);
     }
 }
