95df6f36e0c3b1781dd3b35b7ecddf2646eb1ca8
scolphew <scolphew@pku.edu.cn>
2017-06-08 21:38:04 +0800

    442 1 ≤ a[i] ≤ n(n = size of array)有些元素出现一次，有些两次，输出两次的

--- src/lsf/_442_FindAllDuplicatesinanArray/Solution.java
+++ src/lsf/_442_FindAllDuplicatesinanArray/Solution.java
@@ -24,8 +24,32 @@ public class Solution {
         return ans;
     }
 
+
+    public List<Integer> foo(int[] nums) {
+        List<Integer> duplicates = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > nums.length) {
+                int originalNumber = nums[i] - nums.length;
+                if (originalNumber <= nums.length)
+                    nums[originalNumber - 1] += nums.length;
+                else
+                    nums[originalNumber - nums.length - 1] += nums.length;
+            } else {
+                nums[nums[i] - 1] += nums.length;
+            }
+        }
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] -= (2 * nums.length);
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > 0)
+                duplicates.add(i + 1);
+        }
+        return duplicates;
+    }
+
     public static void main(String[] args) {
-        System.out.println(new Solution().findDuplicates(new int[]{4, 3, 2, 7, 8, 2, 3, 1}));
+        System.out.println(new Solution().foo(new int[]{4, 7, 8, 2, 3, 2, 3, 1}));
         System.out.println(new Solution().findDuplicates(new int[]{10, 2, 5, 10, 9, 1, 1, 4, 3, 7}));
     }
 }
