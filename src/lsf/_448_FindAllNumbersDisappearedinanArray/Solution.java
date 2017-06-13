97b71c04ca2d309e2709f562309a341edea82004
scolphew <scolphew@pku.edu.cn>
2017-06-13 18:49:30 +0800

    448 同442找出长度n的数组中（元素仅为1-n，有的一个，有的两个）找出没出现

---
+++ src/lsf/_448_FindAllNumbersDisappearedinanArray/Solution.java
@@ -0,0 +1,38 @@
+package lsf._448_FindAllNumbersDisappearedinanArray;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        int n = nums.length;
+        List<Integer> list = new ArrayList<>(n);
+        for (int i = 0; i < n; i++) {
+            if (nums[i] <= n) {
+                nums[nums[i] - 1] += n;
+            } else {
+                int odd_ = nums[i] - n;
+                if (odd_ <= n) {
+                    nums[odd_ - 1] += n;
+                } else {
+                    nums[odd_ - 1 - n] += n;
+                }
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            nums[i] -= 2 * n;
+        }
+        for (int i = 0; i < n; i++) {
+            if (nums[i] <= -n) {
+                list.add(i + 1);
+            }
+        }
+        return list;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Solution().findDisappearedNumbers(new int[]{4, 3, 2, 7, 8, 2, 3, 1}));
+        System.out.println(new Solution().findDisappearedNumbers(new int[]{4, 3, 2, 7, 7, 2, 3, 1}));
+    }
+
+}
