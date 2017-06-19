683c0f28abc97e640fac654faa0023e26e76cab7
scolphew <scolphew@pku.edu.cn>
2017-06-19 12:35:00 +0800

    477 求数组中两两二进制距离之和 Input: 4, 14, 2 Output: 6 4 is 0100, 14 is 1110, and 2 is 0010 HammingDistance(4,14)+HammingDistance(4,2)+HammingDistance(14,2)=2+2+2=6

---
+++ src/lsf/_477_TotalHammingDistance/Solution.java
@@ -0,0 +1,24 @@
+package lsf._477_TotalHammingDistance;
+
+public class Solution {
+
+    public int totalHammingDistance(int[] nums) {
+        int n = nums.length;
+        int ans = 0;
+        for (int i = 0; i < 32; i++) {
+            int count_1 = 0;
+            for (int j = 0; j < n; j++) {
+                if ((nums[j] & 1) == 1)
+                    count_1++;
+                nums[j] >>= 1;
+            }
+            ans += count_1 * (n - count_1);
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Solution().totalHammingDistance(new int[]{0xffffffff, 0}));
+    }
+
+}
