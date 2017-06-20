766160c2335068974532acfe181db54a49ceeac9
scolphew <scolphew@pku.edu.cn>
2017-06-20 15:40:43 +0800

    481 将字符串s去掉‘-’,分成若干段，用‘-’连接，每段k个。第一段除外（除不进的情况下剩下的部分），

---
+++ src/lsf/_482_LicenseKeyFormatting/Solution.java
@@ -0,0 +1,24 @@
+package lsf._482_LicenseKeyFormatting;
+
+public class Solution {
+
+    public String licenseKeyFormatting(String S, int K) {
+        S = S.replaceAll("-", "").toUpperCase();
+        int firstLength = S.length() % K;
+        StringBuilder sb = new StringBuilder(S);
+        int nIndex = firstLength;
+        if (firstLength == 0) {
+            nIndex = K;
+        }
+        while (nIndex < sb.length()) {
+            sb.insert(nIndex, '-');
+            nIndex += K + 1;
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Solution().licenseKeyFormatting("adc123-123", 4));
+    }
+}
+
