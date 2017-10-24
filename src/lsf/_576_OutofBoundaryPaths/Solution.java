7edeb88aa80309fbf4b8b1cd033950de6fc589c1
scolphew <scolphew@pku.edu.cn>
2017-10-24 14:24:45 +0800

    576 m*n的棋盘，小球位于i，j位置，最多走N步 有多少种可能 出棋盘就结束，没有边缘保护

---
+++ src/lsf/_576_OutofBoundaryPaths/Solution.java
@@ -0,0 +1,49 @@
+package lsf._576_OutofBoundaryPaths;
+
+import java.util.Arrays;
+
+public class Solution {
+    public int findPaths(int m, int n, int N, int i, int j) {
+        if (N <= 0) return 0;
+
+        final int MOD = 1000000007;
+        int[][] count = new int[m][n];
+        int[][] temp = new int[m][n];
+        count[i][j] = 1;
+        int result = 0;
+
+        int[][] dirs = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+
+        for (int step = 0; step < N; step++) {
+            for (int[] ints : temp) {
+                Arrays.fill(ints, 0);
+            }
+            for (int r = 0; r < m; r++) {
+                for (int c = 0; c < n; c++) {
+                    if (count[r][c] != 0) {
+                        for (int[] d : dirs) {
+                            int nr = r + d[0];
+                            int nc = c + d[1];
+                            if (nr < 0 || nr >= m || nc < 0 || nc >= n) {
+                                result = (result + count[r][c]) % MOD;
+                            } else {
+                                temp[nr][nc] = (temp[nr][nc] + count[r][c]) % MOD;
+                            }
+                        }
+                    }
+                }
+            }
+            int[][] x = count;
+            count = temp;
+            temp = x;
+        }
+        return result;
+    }
+
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.findPaths(1, 3, 3, 0, 1));
+    }
+
+}
