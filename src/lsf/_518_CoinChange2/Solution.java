f6f92a7b3d297093e1fd3b3b234dcd1a2d4414bf
scolphew <scolphew@pku.edu.cn>
2018-05-08 19:36:49 +0800

    518排列组合 求用coins数组中的数组成amount的组成种类，可以重复使用 ex: 5=5 5=2+2+1 5=2+1+1+1 5=1+1+1+1+1

---
+++ src/lsf/_518_CoinChange2/Solution.java
@@ -0,0 +1,19 @@
+package lsf._518_CoinChange2;
+
+public class Solution {
+    public int change(int amount, int[] coins) {
+        int[] dp = new int[amount + 1];
+        dp[0] = 1;
+        for (int j : coins) {
+            for (int i = j; i <= amount; i++) {
+                dp[i] += dp[i - j];
+            }
+        }
+        return dp[amount];
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.change(100, new int[]{1, 2, 3, 4, 5}));
+    }
+}
