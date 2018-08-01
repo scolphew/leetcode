41db4804aa72b4718ce2aaa6c132ab50b594af7d
scolphew <scolphew@pku.edu.cn>
2018-08-01 10:26:27 +0800

    树的先序遍历

--- src/base/Tree/Node.java
+++ src/base/Tree/Node.java
@@ -1,5 +1,7 @@
 package base.Tree;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Node {
@@ -13,4 +15,21 @@ public class Node {
         val = _val;
         children = _children;
     }
+
+    @Override
+    public String toString() {
+        return val + "";
+    }
+
+    public static Node example = new Node(
+            1,
+            Arrays.asList(
+                    new Node(3, Arrays.asList(
+                            new Node(5, new ArrayList<>()),
+                            new Node(6, new ArrayList<>())
+                    )),
+                    new Node(2, new ArrayList<>()),
+                    new Node(4, new ArrayList<>())
+            )
+    );
 }
