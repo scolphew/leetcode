31a98ad1e49173b4e8355de45a2311b649375a94
scolphew <scolphew@pku.edu.cn>
2018-09-27 17:11:44 +0800

    669 修剪二叉树，只保留二叉树中LR之间的结点，返回根节点

---
+++ src/lsf/java/_669_TrimaBinarySearchTree/Solution.java
@@ -0,0 +1,20 @@
+package lsf.java._669_TrimaBinarySearchTree;
+
+import base.TreeNode;
+
+public class Solution {
+
+    public TreeNode trimBST(TreeNode root, int L, int R) {
+        if (root == null) {
+            return null;
+        }
+        if (root.val < L) {
+            return trimBST(root.right, L, R);
+        } else if (root.val > R) {
+            return trimBST(root.left, L, R);
+        }
+        root.left = trimBST(root.left, L, R);
+        root.right = trimBST(root.right, L, R);
+        return root;
+    }
+}
