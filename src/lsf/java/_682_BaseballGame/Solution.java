d431aa77d60d23a3766e5c98d3acaeaf7af05e82
scolphew <scolphew@pku.edu.cn>
2019-03-03 17:21:25 +0800

    683 给定一个字符串列表，每个字符串可以是以下四种类型之一：     1.整数（一轮的得分）：直接表示您在本轮中获得的积分数。     2. "+"（一轮的得分）：表示本轮获得的得分是前两轮有效 回合得分的总和。     3. "D"（一轮的得分）：表示本轮获得的得分是前一轮有效 回合得分的两倍。     4. "C"（一个操作，这不是一个回合的分数）：表示您获得的最后一个有效 回合的分数是无效的，应该被移除。
        每一轮的操作都是永久性的，可能会对前一轮和后一轮产生影响。
        你需要返回你在所有回合中得分的总和。

---
+++ src/lsf/java/_682_BaseballGame/Solution.java
@@ -0,0 +1,31 @@
+package lsf.java._682_BaseballGame;
+
+import java.util.Stack;
+
+public class Solution {
+
+    public int calPoints(String[] ops) {
+        Stack<Integer> stack = new Stack();
+
+        for (String s : ops) {
+            if ("+".equals(s)) {
+                int a = stack.pop();
+                int b = a + stack.peek();
+                stack.push(a);
+                stack.push(b);
+            } else if ("C".equals(s)) {
+                stack.pop();
+            } else if ("D".equals(s)) {
+                stack.push(2 * stack.peek());
+            } else {
+                stack.push(Integer.valueOf(s));
+            }
+        }
+
+        int ans = 0;
+        for (int score : stack)
+            ans += score;
+        return ans;
+    }
+
+}
