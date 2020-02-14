2b965c0c64f5dd8d529078ff5e19a6a905a0ce41
scolphew <scolphew@pku.edu.cn>
2020-02-14 20:29:43 +0800

    704 二分搜索

---
+++ src/lsf/java/_704_BinarySearch/Solution.java
@@ -0,0 +1,28 @@
+package lsf.java._704_BinarySearch;
+
+public class Solution {
+
+    public int search(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + ((right - left) >>> 1);
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.search(
+                new int[]{-1, 0, 3, 5, 9, 12},
+                2
+        ));
+    }
+
+}
