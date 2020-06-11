15d12310b7d7eb662d07ce5335667e0f00b68196
scolphew <scolphew@pku.edu.cn>
2020-06-11 09:54:14 +0800

    731 日历2

---
+++ src/lsf/java/_07_/_731_MyCalendarII/MyCalendarTwo.java
@@ -0,0 +1,39 @@
+package lsf.java._07_._731_MyCalendarII;
+
+
+import java.util.TreeMap;
+
+public class MyCalendarTwo {
+
+    TreeMap<Integer, Integer> delta;
+
+    public MyCalendarTwo() {
+        delta = new TreeMap();
+    }
+
+    public boolean book(int start, int end) {
+        delta.put(start, delta.getOrDefault(start, 0) + 1);
+        delta.put(end, delta.getOrDefault(end, 0) - 1);
+
+        int active = 0;
+        for (int d : delta.values()) {
+            active += d;
+            if (active >= 3) {
+                delta.put(start, delta.get(start) - 1);
+                delta.put(end, delta.get(end) + 1);
+                if (delta.get(start) == 0)
+                    delta.remove(start);
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        MyCalendarTwo calendar = new MyCalendarTwo();
+        calendar.book(1, 100);
+        calendar.book(5, 96);
+        calendar.book(3, 98);
+    }
+
+}
